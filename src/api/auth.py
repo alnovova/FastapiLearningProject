9ecbf382785644da3233922e20ff32733e5ca569
Alesia Novova <a.novova@qsoft.ru>
2025-05-29 12:49:28 +0200

    JWT token (lesson)

--- src/api/auth.py
+++ src/api/auth.py
@@ -1,40 +1,36 @@
-from fastapi import APIRouter, HTTPException
-from fastapi.encoders import jsonable_encoder
-from passlib.context import CryptContext
+from fastapi import APIRouter, HTTPException, Response
 from sqlalchemy.exc import IntegrityError
-from starlette import status
-from starlette.responses import JSONResponse
 
 from src.database import async_session_maker
 from src.repositories.users import UsersRepository
 from src.schemas.users import UserRequestAdd, UserAdd
+from src.services.auth import AuthService
 
 
 router = APIRouter(prefix="/auth", tags=["Авторизация и аутентификация"])
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
 
 @router.post("/register")
 async def register_user(data: UserRequestAdd):
-    hashed_password = pwd_context.hash(data.password)
+    hashed_password = AuthService().hash_password(data.password)
     new_user_data = UserAdd(email=data.email, hashed_password=hashed_password)
     try:
         async with async_session_maker() as session:
             await UsersRepository(session).add(new_user_data)
             await session.commit()
+            return {"status": "OK"}
     except IntegrityError:
-        return JSONResponse(
-            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
-            content=jsonable_encoder({
-                "detail": [
-                    {
-                        "loc": ["body", "email"],
-                        "msg": "Email уже зарегистрирован",
-                        "type": "value_error.unique"
-                    }
-                ]
-            })
-        )
+        raise HTTPException(status_code=422, detail="Email уже зарегистрирован")
 
-    return {"status": "OK"}
+
+@router.post("/login")
+async def login_user(data: UserRequestAdd, response: Response):
+    async with async_session_maker() as session:
+        user = await UsersRepository(session).get_user_with_hashed_password(email=data.email)
+        if not user:
+            raise HTTPException(status_code=401, detail="Пользователь с таким email не зарегистрирован")
+        if not AuthService().verify_password(data.password, user.hashed_password):
+            raise HTTPException(status_code=401, detail="Пароль не верный")
+        access_token = AuthService().create_access_token({"user_id": user.id})
+        response.set_cookie("access_token", access_token)
+        return {"access_token": access_token}
