2d8099bfb3c769089940a177caed9d9b24251e40
Alesia Novova <a.novova@qsoft.ru>
2025-06-05 19:30:58 +0200

    Queries (lesson)

--- src/api/hotels.py
+++ src/api/hotels.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 from fastapi import Query, APIRouter, Body
 
 from src.api.dependencies import PaginationDep, DBDep
@@ -11,14 +13,13 @@ async def get_hotels(
         pagination: PaginationDep,
         db: DBDep,
         title: str | None = Query(None, description="Название отеля"),
-        location: str | None = Query(None, description="Адрес")
+        location: str | None = Query(None, description="Адрес"),
+        date_from: date = Query(example="2025-05-30"),
+        date_to: date = Query(example="2025-06-12")
 ):
-    per_page = pagination.per_page or 5
-    return await db.hotels.get_all(
-        title=title,
-        location=location,
-        limit=per_page,
-        offset=per_page * (pagination.page - 1)
+    return await db.hotels.get_filtered_by_time(
+        date_from=date_from,
+        date_to=date_to
     )
 
 
--- src/repositories/rooms.py
+++ src/repositories/rooms.py
@@ -1,5 +1,8 @@
+from datetime import date
+
 from src.models.rooms import RoomsORM
 from src.repositories.base import BaseRepository
+from src.repositories.utils import rooms_ids_for_booking
 from src.schemas.rooms import Room
 
 
@@ -7,3 +10,7 @@ class RoomsRepository(BaseRepository):
 
     model = RoomsORM
     schema = Room
+
+    async def get_filtered_by_time(self, hotel_id: int, date_from: date, date_to: date):
+        rooms_ids_to_get = rooms_ids_for_booking(date_from=date_from, date_to=date_to, hotel_id=hotel_id)
+        return await self.get_filtered(RoomsORM.id.in_(rooms_ids_to_get))
