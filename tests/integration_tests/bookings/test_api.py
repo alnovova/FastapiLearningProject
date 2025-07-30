1ecc840da37f5ad478b11ce2c33400ff0b9a789c
Alesia Novova <a.novova@qsoft.ru>
2025-07-30 16:32:25 +0200

    Parametrized test (lesson)

--- tests/integration_tests/bookings/test_api.py
+++ tests/integration_tests/bookings/test_api.py
@@ -1,17 +1,27 @@
+import pytest
 
 
-async def test_add_booking(db, authenticated_ac):
-    room_id = (await db.rooms.get_all())[0].id
+@pytest.mark.parametrize("room_id, date_from, date_to, status_code", [
+    (1, "2024-08-01", "2024-08-10", 200),
+    (1, "2024-08-01", "2024-08-10", 200),
+    (1, "2024-08-01", "2024-08-10", 200),
+    (1, "2024-08-01", "2024-08-10", 200),
+    (1, "2024-08-01", "2024-08-10", 200),
+    (1, "2024-08-01", "2024-08-10", 400),
+    (1, "2024-09-01", "2024-09-10", 200),
+])
+async def test_add_booking(room_id, date_from, date_to, status_code, db, authenticated_ac):
     response = await authenticated_ac.post(
         "/bookings",
         json={
             "room_id": room_id,
-            "date_from": "2024-08-01",
-            "date_to": "2024-08-10",
+            "date_from": date_from,
+            "date_to": date_to,
         }
     )
-    assert response.status_code == 200
-    result = response.json()
-    assert isinstance(result, dict)
-    assert result["status"] == "OK"
-    assert "data" in result
+    assert response.status_code == status_code
+    if response.status_code == 200:
+        result = response.json()
+        assert isinstance(result, dict)
+        assert result["status"] == "OK"
+        assert "data" in result
